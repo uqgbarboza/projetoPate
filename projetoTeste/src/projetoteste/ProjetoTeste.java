9db3c89fa43441dc9dcea526912727f197846309
gbarboza <gbarboza@DF0019661.uniaoquimica.com.br>
2017-09-04 11:24:44 -0300

    msgbox adicionado

--- projetoTeste/src/projetoteste/ProjetoTeste.java
+++ projetoTeste/src/projetoteste/ProjetoTeste.java
@@ -1,7 +1,10 @@
 package projetoteste;
 
+import javax.swing.JOptionPane;
 public class ProjetoTeste {
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Teste");
     }
-    
+        
+                
 }
